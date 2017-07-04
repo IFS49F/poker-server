ba2aef85fbe3c6a2d05011d95d3f63762eddf10a
just4fun <houritsunohikari@gmail.com>
2017-07-04 18:31:07 +0800

    use redis to store the session

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const Redis = require('ioredis');
 const port = process.env.PORT || 4000;
 
 app.use(express.static(__dirname + '/public'));
 
 let clients = {};
+let redis = new Redis();
 
 function onConnection(socket) {
   console.log('Client ' + socket.id + ' connected...');
@@ -19,77 +21,104 @@ function onConnection(socket) {
     currentRoom = room;
 
     socket.join(room, () => {
-      if (!clients[room]) {
-        console.log('New room ' + room + ' created for the first client...');
-        clients[room] = {
-          team: [],
-          show: false
-        };
-      }
+      redis.get(room, (err, result) => {
+        if (!result) {
+          console.log('New room ' + room + ' created for the first client...');
+          result = {
+            team: [],
+            show: false
+          }
+        } else {
+          result = JSON.parse(result);
+        }
 
-      clients[room].team.push({
-        id: socket.id,
-        name: playerName,
-        score: null,
-        voted: false
+        result.team.push({
+          id: socket.id,
+          name: playerName,
+          score: null,
+          voted: false
+        });
+
+        redis.set(room, JSON.stringify(result));
+        // sending to all clients in room include sender
+        io.in(room).emit('joined', result);
       });
-
-      // sending to all clients in room include sender
-      io.in(room).emit('joined', clients[room]);
     });
   });
 
   socket.on('vote', (score) => {
-    if (!clients[currentRoom]) { return; }
+    redis.get(currentRoom, (err, result) => {
+      if (!result) { return; }
 
-    console.log('Client ' + socket.id + ' of room ' + currentRoom + ' voted...');
+      result = JSON.parse(result);
 
-    let votingPlayer = clients[currentRoom].team.find(item => item.id === socket.id);
+      console.log('Client ' + socket.id + ' of room ' + currentRoom + ' voted...');
 
-    votingPlayer.score = score;
-    votingPlayer.voted = true;
+      let votingPlayer = result.team.find(item => item.id === socket.id);
 
-    io.in(currentRoom).emit('voted', clients[currentRoom]);
+      votingPlayer.score = score;
+      votingPlayer.voted = true;
+
+      redis.set(currentRoom, JSON.stringify(result));
+      io.in(currentRoom).emit('voted', result);
+    });
   });
 
   socket.on('show', (show) => {
-    if (!clients[currentRoom]) { return; }
+    redis.get(currentRoom, (err, result) => {
+      if (!result) { return; }
 
-    console.log('Client ' + socket.id + ' of room ' + currentRoom + ' showed the result...');
+      result = JSON.parse(result);
 
-    clients[currentRoom].show = show;
+      console.log('Client ' + socket.id + ' of room ' + currentRoom + ' showed the result...');
 
-    io.in(currentRoom).emit('voted', clients[currentRoom]);
+      result.show = show;
+
+      redis.set(currentRoom, JSON.stringify(result));
+      io.in(currentRoom).emit('voted', result);
+    });
   });
 
   socket.on('clear', (show) => {
-    if (!clients[currentRoom]) { return; }
+    redis.get(currentRoom, (err, result) => {
+      if (!result) { return; }
 
-    console.log('Client ' + socket.id + ' of room ' + currentRoom + ' cleared the result...');
+      result = JSON.parse(result);
 
-    clients[currentRoom].team.forEach(item => {
-      item.score = null;
-      item.voted = false;
+      console.log('Client ' + socket.id + ' of room ' + currentRoom + ' cleared the result...');
+
+      result.team.forEach(item => {
+        item.score = null;
+        item.voted = false;
+      });
+      result.show = false;
+
+      redis.set(currentRoom, JSON.stringify(result));
+      io.in(currentRoom).emit('cleared', result);
     });
-    clients[currentRoom].show = false;
-
-    io.in(currentRoom).emit('cleared', clients[currentRoom]);
   });
 
   socket.on('disconnect', () => {
-    if (!clients[currentRoom]) { return; }
+    redis.get(currentRoom, (err, result) => {
+      if (!result) { return; }
 
-    console.log('Client ' + socket.id + ' of room ' + currentRoom + ' disconnected...');
+      result = JSON.parse(result);
 
-    clients[currentRoom].team = clients[currentRoom].team.filter(item => item.id !== socket.id);
+      console.log('Client ' + socket.id + ' of room ' + currentRoom + ' disconnected...');
 
-    if (clients[currentRoom].team.length === 0) {
-      console.log('All clients in disconnected, close room ' + currentRoom);
-      delete clients[currentRoom];
-    }
+      result.team = result.team.filter(item => item.id !== socket.id);
 
-    io.in(currentRoom).emit('disconnected', clients[currentRoom]);
-  })
+      if (result.team.length === 0) {
+        console.log('All clients in disconnected, close room ' + currentRoom);
+        result = null;
+        redis.del(currentRoom);
+      } else {
+        redis.set(currentRoom, JSON.stringify(result));
+      }
+
+      io.in(currentRoom).emit('disconnected', result);
+    });
+  });
 }
 
 io.on('connection', onConnection);
